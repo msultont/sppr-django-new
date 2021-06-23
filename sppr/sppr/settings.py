f764a09c07c04e5de072402ef08d16170cff7a5d
amrullahfathur <amrullahfathur@gmail.com>
2021-06-23 23:19:53 +0700

    setting database
    sip

--- sppr/sppr/settings.py
+++ sppr/sppr/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'sppr.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sppr',
-        'USER': 'fauziabd',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432'
+'default':
+
+{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+
+    }
+
 
 
 # Password validation
