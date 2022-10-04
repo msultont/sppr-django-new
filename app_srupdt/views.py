fb09ef7b4501ab8297f097385259fd29733f7db5
Sulton <msulton55@gmail.com>
2022-10-04 17:29:08 +0700

    mst-24-uploader

--- app_srupdt/views.py
+++ app_srupdt/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
+
     return render(request, "index_srupdt.html")
+
+
+def uploader(request):
+
+    return render(request, "uploader.html")
--- sppr/urls.py
+++ sppr/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('app/sppr/', include('app_sppr.urls')),
     path('app/srupdt/', include('app_srupdt.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
