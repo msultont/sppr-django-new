fd5eb8b3201a27fb85cc5b9b01555475ce0a8f08
Vermaquel <66999041+Vermaquel@users.noreply.github.com>
2022-10-17 09:33:00 +0700

    [MST-42] container-dan-isi-untuk-menu-album-tematik-sipasti
    change log:
    1. Pembuatan container card untuk isi album tematik,
    2. Pembuatan container search untuk mencari album tematik,
    3. Pembuatan container checklist untuk mencari berdasarkan provinsi,
    4. Pembaharuan link untuk menuju ke album tematik
    5. Pembaharuan url akses app sipasti
    6. Pembaharuan views logic app sipasti

--- app_sipasti/urls.py
+++ app_sipasti/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index_sipasti"),
+    path('albumtematik/', views.albumtematik, name="albumtematik"),
 ]
--- app_sipasti/views.py
+++ app_sipasti/views.py
@@ -2,6 +2,58 @@ import json
 from django.shortcuts import render
 
 
-# Create your views here.
+def getData():
+    province_list = ['Aceh',
+                     'Sumatera Utara',
+                     'Sumatera Barat',
+                     'Riau',
+                     'Jambi',
+                     'Sumatera Selatan',
+                     'Bengkulu',
+                     'Lampung',
+                     'Kep. Bangka Belitung',
+                     'Kep. Riau',
+                     'DKI Jakarta',
+                     'Jawa Barat',
+                     'Jawa Tengah',
+                     'DI Yogyakarta',
+                     'Jawa Timur',
+                     'Banten',
+                     'Bali']
+
+    dummyData = [{
+                    "tittle": "Kemacetan Ulapan",
+                    "img": "https://www.baliaround.com/wp-content/uploads/2013/09/monkey-forest-ubud.jpg",
+                    "text": "Analisis kebutuhan infrastruktur transportasi dalam mengatasi isu dan permasalahan kemacetan, kurangnya lahan parkir, minimnya layanan transportasi umum berdasarkan dokumen perencanaan yang ada, seperti Rencana Aksi dalam Rencana Induk Terpadu (Integrated Master Plan) Pengembangan Kawasan Pariwisata Ulapan.",
+                    "province": "Bali"
+                 },
+                 {
+                    "tittle": "Pelabuhan Industri Lampung",
+                    "img": "https://logisklik.com/blog/wp-content/uploads/2022/05/panjang2.jpg",
+                    "text": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    "province": "Lampung"
+                 },
+                 {
+                    "tittle": "Geopark Karangsambung",
+                    "img": "https://www.goodnewsfromindonesia.id/uploads/images/2020/07/2115342020-Goodnewsfromindonesia-GNFI-Sungai-Luk-Ulo.jpg",
+                    "text": "Geopark atau taman bumi merupakan sebuah wilayah geografi tunggal atau gabungan, yang memiliki situs warisan geologi(geosite) dan bentang alam yang bernilai, terkait aspek warisan geologi, keanekaragaman geologi, hayati dan budaya.",
+                    "province": "Jawa Tengah"
+                 },
+                 {
+                    "tittle": "Isu Strategis: Tingginya resiko bencana di pantura jawa tengah",
+                    "img": "https://i.ytimg.com/vi/5Ee_8hXCur8/maxresdefault.jpg",
+                    "text": "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                    "province": "Jawa Tengah"
+                 }]
+
+    return {"a": province_list, "b": dummyData}
+
+
 def index(request):
     return render(request, "index_sipasti.html")
+
+
+def albumtematik(request):
+    data = getData()
+    return render(request, "albumtematik/albumtematik.html", {"province": json.dumps(data['a']),
+                                                              "contentD": json.dumps(data['b'])})
